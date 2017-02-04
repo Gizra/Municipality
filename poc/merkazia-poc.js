a0d29e81565aa30aa672458b807297347aaf1874
Noam <noamoss@gmail.com>
2017-02-04 13:11:27 +0200

    add expected API response to merkazia-poc.js (in comment)

--- poc/merkazia-poc.js
+++ poc/merkazia-poc.js
@@ -1,5 +1,5 @@
 var quriesResults = [
-  { title: 'איך משלמים ארנונה' },
+  { title: 'איך משלמים ארנונה'},
   { title: 'עד מתי אפשר להירשם לגנים' },
   { title: 'להגיש תלונה' },
   { title: 'למלא טופס פטור מארנונה' },
@@ -11,6 +11,8 @@ var quriesResults = [
   { title: 'תקלה בכביש' },
   { title: 'בעיה בגן השעשועים' },
   { title: 'הצפה של ביוב' },
+  { title: 'איך מוציאים רישיון עסק' },
+  { title: 'מה עשתה העיריה השנה' },
   // etc
 ];
 
@@ -18,3 +20,20 @@ var quriesResults = [
 $('.ui.search').search({
     source: quriesResults
     });
+
+/* expected Json on server API:
+{ question: 'איך משלמים ארנונה', link: 'services/arnona/pay', category: "לתושב", item: "תשלום ארנונה" },
+{ question: 'עד מתי אפשר להירשם לגנים', link:'services/education/register',category: "לתושב" },
+{ question: 'להגיש תלונה', link:'services/contact/moked',category: "לתושב"},
+{ question: 'למלא טופס פטור מארנונה', link: 'services/arnona/ptor',category: "לתושב" },
+{ question: 'לתאם פגישה', link: 'information/contacts', category: "לתושב" },
+{ question: 'לדווח על מפגע', link: 'services/contact/moked',category: "לתושב" },
+{ question: 'תלונה על רעש', link: 'services/contact/moked', category: "לתושב" },
+{ question: 'הרשמה לגנים', link: 'services/education/register',category: "לתושב" },
+{ question: 'דו"ח כספי', link: 'information/reports/budget', category: "לתושב" },
+{ question: 'תקלה בכביש', link: 'services/contact/moked', category: "לתושב" },
+{ question: 'בעיה בגן השעשועים', link: 'services/contact/moked', category: "לתושב" },
+{ question: 'הצפה של ביוב', link: 'services/contact/moked', category: "לתושב" },
+{ question: 'איך מוציאים רישיון עסק', link: 'services/buisness/license', category: "עסקים" },
+{ question: 'מה עשתה העיריה השנה',link: 'information/reports/annual_report' category: "הרשות" },
+*/
