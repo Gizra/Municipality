c8bb01225225fce1a84cd6ad4746136eb711113c
Itamar Bar-Lev Shapira <bitamar@gmail.com>
2017-03-10 20:54:02 +0100

    Displaying municipality news

---
+++ server/hedley/modules/custom/hedley_municipality/plugins/content_types/promoted_content/promoted-content.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Promoted content template.
+ */
+?>
+<div class="ui link items">
+  <?php print render($nodes); ?>
+
+  <?php print $all_nodes_link; ?>
+</div>
--- server/hedley/themes/custom/hedley_theme/template.php
+++ server/hedley/themes/custom/hedley_theme/template.php
@@ -4,3 +4,20 @@
  * @file
  * Hedley theme template file.
  */
+
+/**
+ * Preprocess node.
+ *
+ * Add a theme suggestion including the bundle and view mode.
+ */
+function hedley_theme_preprocess_node(&$variables) {
+  $suggestion = 'node__' . $variables['type'] . '__' . $variables['view_mode'];
+  // Prepend the template suggestion.
+  array_unshift($variables['theme_hook_suggestions'], $suggestion);
+
+  // Allow a custom preprocess function for this bundle and view mode.
+  $preprocess_function = 'hedley_theme_preprocess_' . $suggestion;
+  if (function_exists($preprocess_function)) {
+    $preprocess_function($variables);
+  }
+}
