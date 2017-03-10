45880300525f9bd9d605f4dd7a31a2f0b5f3edff
Itamar Shapira Bar-Lev <itamar@gizra.com>
2017-03-10 22:34:03 +0100

    Display promoted municipality content (#128)
    * Generic promoted conetnt plugin for municipalities
    
    * Ignoring no variables in t validation
    
    * Displaying municipality news
    
    * Displaying municipality actions + Adding stackable layout
    
    * Displaying municipality events

--- server/hedley/themes/custom/hedley_theme/template.php
+++ server/hedley/themes/custom/hedley_theme/template.php
@@ -4,3 +4,29 @@
  * @file
  * Hedley theme template file.
  */
+
+/**
+ * Preprocess node.
+ *
+ * Add a theme suggestion including the bundle and view mode.
+ */
+function hedley_theme_preprocess_node(&$variables) {
+  $suggestion = 'node__' . $variables['type'] . '__' . $variables['view_mode'];
+  // Prepend the template suggestion.
+  array_unshift($variables['theme_hook_suggestions'], $suggestion);
+
+  // Allow a custom preprocess function for this bundle and view mode.
+  $preprocess_function = 'hedley_theme_preprocess_' . $suggestion;
+  if (function_exists($preprocess_function)) {
+    $preprocess_function($variables);
+  }
+}
+
+/**
+ * Theme override.
+ *
+ * Remove the default panel separators.
+ */
+function hedley_theme_panels_default_style_render_region($variables) {
+  return implode('', $variables['panes']);
+}
---
+++ server/hedley/themes/custom/hedley_theme/templates/node--action--homepage_teaser.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Action node homepage-teaser template.
+ */
+?>
+<div class="item action homepage-teaser">
+  <div class="header">
+    <?php print render($content); ?>
+  </div>
+  <div class="description"></div>
+</div>
