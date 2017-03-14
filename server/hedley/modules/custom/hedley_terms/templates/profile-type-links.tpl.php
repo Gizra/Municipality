c49882f8850cbc23acf96e08c2a262dd57d1f6f0
Itamar Bar-Lev Shapira <bitamar@gmail.com>
2017-03-14 22:17:03 +0100

    Chaining profile query to all urls

--- server/hedley/modules/custom/hedley_terms/templates/profile-type-links.tpl.php
+++ server/hedley/modules/custom/hedley_terms/templates/profile-type-links.tpl.php
@@ -8,7 +8,7 @@
 <div class="ui <?php print $count_class; ?> item menu">
   <?php foreach ($links as $link): ?>
     <a
-      class="item <?php if ($link['active']): ?>active<?php endif; ?>"
+      class="item <?php print $link['active'] ? 'active' : ''; ?>"
       href="<?php print $link['url']; ?>"
     >
       <?php print $link['label']; ?>
