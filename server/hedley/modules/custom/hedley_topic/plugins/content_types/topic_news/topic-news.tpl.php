13589996852002436f4dc1723f5fb271f003f793
Itamar Bar-Lev Shapira <bitamar@gmail.com>
2017-03-15 18:40:55 +0100

    Displaying news by profile type / topic / muni

--- server/hedley/modules/custom/hedley_topic/plugins/content_types/topic_news/topic-news.tpl.php
+++ server/hedley/modules/custom/hedley_topic/plugins/content_types/topic_news/topic-news.tpl.php
@@ -10,14 +10,7 @@
     <h3 class="ui purple header"><?php print t('News regarding @topic', ['@topic' => $title]); ?></h3>
   </div>
 
-
-  <div class="ui positive message row">שוברים לתשלום שנתי נשלחו לכל בתי האב</div>
-
-
-
-  <div class="ui positive message row">הנחה של  5% למשלמים בכרטיס אשראי</div>
-
-
-
-
+  <?php foreach ($news_items as $item): ?>
+    <div class="ui positive message row"><?php print $item; ?></div>
+  <?php endforeach; ?>
 </div>
