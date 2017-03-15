fdb3d9ee077326df3cb3599adf29d26c0cb3f8d3
Itamar Bar-Lev Shapira <bitamar@gmail.com>
2017-03-15 18:08:03 +0100

    Adding topic news plugin

---
+++ server/hedley/modules/custom/hedley_topic/plugins/content_types/topic_news/topic-news.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Topic news.
+ */
+?>
+<div class="ui grid container">
+  <div class="centered row">
+    <h3 class="ui purple header"><?php print t('News regarding @topic', ['@topic' => $title]); ?></h3>
+  </div>
+
+
+  <div class="ui positive message row">שוברים לתשלום שנתי נשלחו לכל בתי האב</div>
+
+
+
+  <div class="ui positive message row">הנחה של  5% למשלמים בכרטיס אשראי</div>
+
+
+
+
+</div>
