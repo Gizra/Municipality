3ee054faa248ee5fa59707072fd44343e9b93f4f
Itamar Shapira Bar-Lev <itamar@gizra.com>
2017-03-16 18:03:54 +0100

    Topic page news (#150)
    * Adding more news examples
    
    * Adding topic news plugin
    
    * Adding topic news plugin
    
    * Displaying news by profile type / topic / muni
    
    * Displaying news in current language + read more  + admin view
    
    * Fixing comment

--- server/hedley/modules/custom/hedley_topic/plugins/content_types/term_title/term-title.tpl.php
+++ server/hedley/modules/custom/hedley_topic/plugins/content_types/term_title/term-title.tpl.php
@@ -5,6 +5,4 @@
  * Term title template.
  */
 ?>
-<div class="ui segment centered row">
-  <h1 class="ui header"><?php print $title; ?></h1>
-</div>
+<h1 class="ui header"><?php print $title; ?></h1>
---
+++ server/hedley/modules/custom/hedley_topic/plugins/content_types/topic_news/topic-news.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Topic news.
+ */
+?>
+<div class="ui grid container">
+  <div class="centered row"">
+    <h3 class="ui purple header"><?php print t('News regarding @topic', ['@topic' => $title]); ?></h3>
+  </div>
+
+  <?php foreach ($news_items as $item): ?>
+    <div class="ui positive message row">
+      <?php print $item['title']; ?>
+      <?php print $item['read_more']; ?>
+    </div>
+  <?php endforeach; ?>
+</div>
