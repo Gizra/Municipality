14e1eea6e4530a913b082eaab86bd9516c80077a
Aron Novak <aron@gizra.com>
2017-05-15 16:56:35 +0200

    ci-scripts: added Simpletest execution (Hedley group does not exist yet)

--- ci-scripts/docker_files/run.sh
+++ ci-scripts/docker_files/run.sh
@@ -8,12 +8,14 @@ if [ -z "$DOCKER_DEBUG" ]; then
   # Regular Travis execution, failing on the first error is what we want.
   bash preparing_server.sh
   bash run_webdriverio.sh
+  bash run_simpletest.sh
 
 else
   # We keep going and open a Bash shell to interactively inspect what failed or
   # what's the status of the application.
   bash preparing_server.sh || :
   bash run_webdriverio.sh || :
+  bash run_simpletest.sh || :
 
   bash
 fi
