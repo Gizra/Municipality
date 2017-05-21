436327e56bdd857dc79bf0a22ef08987aeba8b4d
nader77 <nader@gizra.com>
2017-05-21 11:49:57 +0300

    Improve end date

--- server/hedley/modules/custom/hedley_topic/plugins/content_types/topic_events/topic-events.tpl.php
+++ server/hedley/modules/custom/hedley_topic/plugins/content_types/topic_events/topic-events.tpl.php
@@ -5,11 +5,11 @@
  * Topic events.
  */
 ?>
-<div class="ui four width column cards">
+<div class="ui four column grid cards">
   <h3 class="ui purple header"><?php print t('Events'); ?></h3>
 
   <?php foreach ($events as $event): ?>
-    <div class="card event">
+    <div class="card column event">
       <div class="content">
         <?php if ($event['image_url']): ?>
           <div class="ui tiny image <?php print $event['float_direction']; ?> floated">
@@ -18,7 +18,12 @@
         <?php endif; ?>
         <a href="<?php print $event['url']; ?>" class="header"><?php print $event['title']; ?></a>
         <div class="meta">
-          <?php print t('@date_start to @date_end',['@date_start' => $event['date_start'], '@date_end' => $event['date_end']]); ?>
+          <?php print $event['date_start']; ?>
+          <?php
+          if ($event['date_end']) {
+            print t('to') . ' ' . $event['date_end'];
+          }
+          ?>
         </div>
       </div>
     </div>
