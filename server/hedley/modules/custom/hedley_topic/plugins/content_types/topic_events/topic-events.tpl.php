c793a64cf3554b91486b37c9d7d12d22c154ab7b
nader77 <nader@gizra.com>
2017-05-21 11:19:44 +0300

    Add semantic event

--- server/hedley/modules/custom/hedley_topic/plugins/content_types/topic_events/topic-events.tpl.php
+++ server/hedley/modules/custom/hedley_topic/plugins/content_types/topic_events/topic-events.tpl.php
@@ -5,25 +5,21 @@
  * Topic events.
  */
 ?>
-<div class="four width column">
+<div class="ui four width column cards">
   <h3 class="ui purple header"><?php print t('Events'); ?></h3>
 
   <?php foreach ($events as $event): ?>
-    <div class="item event">
-      <?php if ($event['image_url']): ?>
-        <a class="ui tiny image">
-          <img src="<?php print $event['image_url']; ?>">
-        </a>
-      <?php endif; ?>
-      <div class="middle aligned content">
-        <a class="header"><?php print $event['title']; ?></a>
-        <p>
-          <?php print $event['date_start']; ?>
-        </p>
-        <p><?php print t('to') ?></p>
-        <p>
-          <?php print $event['date_end']; ?>
-        </p>
+    <div class="card event">
+      <div class="content">
+        <?php if ($event['image_url']): ?>
+          <div class="ui tiny image <?php print $event['float_direction']; ?> floated">
+            <img src="<?php print $event['image_url']; ?>">
+          </div>
+        <?php endif; ?>
+        <a href="<?php print $event['url']; ?>" class="header"><?php print $event['title']; ?></a>
+        <div class="meta">
+          <?php print t('@date_start to @date_end',['@date_start' => $event['date_start'], '@date_end' => $event['date_end']]); ?>
+        </div>
       </div>
     </div>
   <?php endforeach; ?>
