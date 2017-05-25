ecf1c51581d456153a639076d165cea0aa9a4ba6
nader77 <nader@gizra.com>
2017-05-25 15:20:37 +0300

    Update formatting [skip ci]

--- elm-app/src/elm/Contact/View.elm
+++ elm-app/src/elm/Contact/View.elm
@@ -46,17 +46,17 @@ viewContacts language { contacts, filterString } =
         filteredContacts =
             filterContacts contacts filterString
     in
-    if DictList.isEmpty filteredContacts then
-        div [] [ text <| translate language ContactsNotFound ]
-    else
-        div [ class "ui link cards" ]
-            (filteredContacts
-                |> DictList.map
-                    (\contactId contact ->
-                        viewContact language ( contactId, contact )
-                    )
-                |> DictList.values
-            )
+        if DictList.isEmpty filteredContacts then
+            div [] [ text <| translate language ContactsNotFound ]
+        else
+            div [ class "ui link cards" ]
+                (filteredContacts
+                    |> DictList.map
+                        (\contactId contact ->
+                            viewContact language ( contactId, contact )
+                        )
+                    |> DictList.values
+                )
 
 
 {-| View a single contact.
