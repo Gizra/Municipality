654eaa7dfffdb43c25b4cfafc6db9ff4808706de
nader77 <nader@gizra.com>
2017-06-01 18:56:13 +0300

    Update DictList package. [skip ci]

--- elm-app/src/elm/Event/Decoder.elm
+++ elm-app/src/elm/Event/Decoder.elm
@@ -4,20 +4,10 @@ module Event.Decoder
         )
 
 import Event.Model exposing (Event, DictListEvent)
-import DictList exposing (DictList, decodeArray, empty)
+import DictList exposing (DictList, decodeArray2, empty)
 import Json.Decode exposing (Decoder, andThen, at, dict, fail, field, float, index, int, keyValuePairs, list, map, map2, nullable, oneOf, string, succeed)
 import Json.Decode.Pipeline exposing (custom, decode, optional, optionalAt, required, requiredAt)
 import Utils.Json exposing (decodeEmptyArrayAs, decodeIntAsString)
-
-
--- @todo: Remove after https://github.com/Gizra/elm-dictlist/pull/10 is merged.
-
-
-decodeArray2 : Decoder comparable -> Decoder value -> Decoder (DictList comparable value)
-decodeArray2 keyDecoder valueDecoder =
-    Json.Decode.map2 (,) keyDecoder valueDecoder
-        |> Json.Decode.list
-        |> Json.Decode.map DictList.fromList
 
 
 decodeEvents : Decoder DictListEvent
