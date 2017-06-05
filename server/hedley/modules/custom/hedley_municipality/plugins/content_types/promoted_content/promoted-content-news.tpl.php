52b8111ccd6a014e9c1e76706a2aa281121efc0b
nader77 <nader@gizra.com>
2017-06-05 17:13:15 +0300

    Add news.

--- server/hedley/modules/custom/hedley_municipality/plugins/content_types/promoted_content/promoted-content-news.tpl.php
+++ server/hedley/modules/custom/hedley_municipality/plugins/content_types/promoted_content/promoted-content-news.tpl.php
@@ -2,19 +2,16 @@
 
 /**
  * @file
- * Events promoted-content template.
+ * News promoted-content template.
  */
 ?>
 <?php foreach ($items as $item): ?>
-  <div class="item event homepage-teaser">
+  <div class="item news homepage-teaser">
     <div class="ui tiny image">
       <img class="ui image thumbnail" src="<?php print $item['image_url']; ?>">
     </div>
-    <div class="middle aligned content">
+    <div class="content">
       <h3 class="header"><?php print $item['title']; ?></h3>
-      <div class="description">
-        <?php print $item['date']; ?>
-      </div>
     </div>
   </div>
 <?php endforeach; ?>
