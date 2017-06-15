d7ab9a9d01189e29daa52aea1f1753b188b31e67
nader77 <nader@gizra.com>
2017-06-15 18:43:35 +0300

    Fix comments

--- wdio/specs/topics.js
+++ wdio/specs/topics.js
@@ -3,20 +3,24 @@ const assert = require('assert');
 describe('Municipality topics', () => {
   before(() => {
     browser.url('/municipality-1/taxonomy/term/13?language=he');
-  })
+  });
 
   it('should show the correct topics', () => {
     browser.waitForVisible('div.pane-topic-actions .buttons');
 
-    // Set expected topics on the page.
-    const expectedTopics = ['לקבל אישור תושב', 'לשלם ארנונה', 'לפנות למוקד'];
+    it('should show "contact customer service"', () => {
+      const topic = browser.getText('div.pane-topic-actions .buttons > a:nth-child(1)');
+      assert.equal('לפנות למוקד', topic);
+    });
 
-    // Get the topics from the page.
-    const topics = browser.getText('div.pane-topic-actions .buttons > a');
+    it('should show "pay municipal rate"', () => {
+      const topic = browser.getText('div.pane-topic-actions .buttons > a:nth-child(2)');
+      assert.equal('לשלם ארנונה', topic);
+    });
 
-    // Assert the page have the expected topics.
-    topics.forEach((topicTitle, index) => {
-      assert.equal(expectedTopics[index], topicTitle);
+    it('should show "Obtain a resident\'s permit"', () => {
+      const topic = browser.getText('div.pane-topic-actions .buttons > a:nth-child(3)');
+      assert.equal('לקבל אישור תושב', topic);
     });
 });
 
