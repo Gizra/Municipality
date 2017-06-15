de17a8e7a0dc530d6fab56f6c4516cb90bff7496
nader77 <nader@gizra.com>
2017-06-15 17:05:33 +0300

    Update theme settings.

--- server/hedley/themes/custom/hedley_theme/template.php
+++ server/hedley/themes/custom/hedley_theme/template.php
@@ -54,8 +54,6 @@ function hedley_theme_panels_default_style_render_region($variables) {
 function hedley_theme_preprocess_page(&$variables) {
   // Add the language switch links.
   $variables['language_switch_links'] = hedley_i18n_language_switch_links();
-  // The class depending on the amount of languages the municipality have.
-  $variables['language_switch_links_class'] = hedley_theme_number_as_word(count($variables['language_switch_links']));
 
   // Add user type links.
   $variables['user_type_links'] = hedley_terms_get_user_type_links();
@@ -88,18 +86,4 @@ function hedley_theme_preprocess_page(&$variables) {
   $variables['last_updated'] = $wrapper->field_last_update->value() ? format_date($wrapper->field_last_update->value(), 'short') : NULL;
   // Footer text.
   $variables['footer_text'] = $wrapper->field_footer_text->value() ? $wrapper->field_footer_text->value->value() : NULL;
-}
-
-/**
- * Get a small number as a word, for semantic ui list classes.
- *
- * @param $number
- *   A number between 0 and 9.
- *
- * @return string
- *   The number as a word.
- */
-function hedley_theme_number_as_word($number) {
-  $numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  return $numbers[$number];
 }
--- server/hedley/themes/custom/hedley_theme/template/page/page--panel.tpl.php
+++ server/hedley/themes/custom/hedley_theme/template/page/page--panel.tpl.php
@@ -13,68 +13,71 @@
 	  
 	  <?php print render($page['before_content']); ?>
 	  <div id="content" class="content full">
-
-          <div class="row">
-            <?php if ($user_type_links): ?>
-              <div class="col-md-2">
-                <?php print $user_type_links; ?>
-              </div>
-            <?php endif; ?>
-
-            <div class="col-md-8">
-              <?php print render($social_links); ?>
+      <div class="container">
+        <div class="row">
+          <?php if ($user_type_links): ?>
+            <div class="col-md-3">
+              <?php print $user_type_links; ?>
             </div>
+          <?php endif; ?>
 
-            <div class="col-md-2">
-              <?php if ($language_switch_links) : ?>
-                <div class="ui <?php print $language_switch_links_class?> item menu languages">
-                  <?php foreach ($language_switch_links as $link): ?>
-                    <?php print $link; ?>
-                  <?php endforeach; ?>
-                </div>
-              <?php endif; ?>
-            </div>
+          <div class="col-md-6">
+            <?php print render($social_links); ?>
           </div>
 
-		      <?php if ($messages): ?>  
-			      <div class="col-md-12">
-		          <?php print $messages; ?>
-			      </div>
-			    <?php endif; ?>  
-			    <?php if ( ($page['sidebar_left']) ) : ?>
-				  <aside id="sidebar-left">
-					  <div class="<?php if ($page['sidebar_right']) { echo "col-md-3";} else { echo "col-md-3"; } ?>">
-					    <div id="sticky-sidebar">
-					    <?php print render($page['sidebar_left']); ?>
-					    </div>
-					  </div>
-				  </aside>
-				  <?php endif; ?>
-			
-					<div class="<?php if ( ($page['sidebar_right']) AND ($page['sidebar_left']) ) { echo "col-md-6";} elseif ( ($page['sidebar_right']) OR ($page['sidebar_left']) ) {  echo "col-md-9"; }  else { echo ""; } ?>">
-					  
-			     	<?php if ($tabs = render($tabs)): ?>
-						  <div id="drupal_tabs" class="tabs ">
-						    <?php print render($tabs); ?>
-						  </div>
-					  <?php endif; ?>
-			      <?php print render($page['help']); ?>
-			      <?php if ($action_links): ?>
-			        <ul class="action-links">
-			          <?php print render($action_links); ?>
-			        </ul>
-			      <?php endif; ?>
-		
-					  <?php if (isset($page['content'])) { print render($page['content']); } ?>
-			      
-					</div>
-			  
-				  <?php if ( ($page['sidebar_right']) ) : ?>
-				  <div class="<?php if ($page['sidebar_left']) { echo "col-md-3";} else { echo "col-md-3"; } ?>">
-				    <?php print render($page['sidebar_right']); ?>
-				  </div>
-				  <?php endif; ?>
-	  </div>  
+          <div class="col-md-3">
+            <?php if ($language_switch_links) : ?>
+              <div class="btn-group" role="group">
+                <?php foreach ($language_switch_links as $link): ?>
+                  <?php print $link; ?>
+                <?php endforeach; ?>
+              </div>
+            <?php endif; ?>
+          </div>
+        </div>
+        <div class="divider"></div>
+      </div>
+      <?php if ($messages): ?>
+        <div class="col-md-12">
+          <?php print $messages; ?>
+        </div>
+      <?php endif; ?>
+      <?php if ( ($page['sidebar_left']) ) : ?>
+      <aside id="sidebar-left">
+        <div class="<?php if ($page['sidebar_right']) { echo "col-md-3";} else { echo "col-md-3"; } ?>">
+          <div id="sticky-sidebar">
+          <?php print render($page['sidebar_left']); ?>
+          </div>
+        </div>
+      </aside>
+      <?php endif; ?>
+
+      <div class="<?php if ( ($page['sidebar_right']) AND ($page['sidebar_left']) ) { echo "col-md-6";} elseif ( ($page['sidebar_right']) OR ($page['sidebar_left']) ) {  echo "col-md-9"; }  else { echo ""; } ?>">
+
+        <div class="container">
+          <?php if ($tabs = render($tabs)): ?>
+            <div id="drupal_tabs" class="tabs">
+              <?php print render($tabs); ?>
+            </div>
+          <?php endif; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?>
+            <ul class="action-links">
+              <?php print render($action_links); ?>
+            </ul>
+          <?php endif; ?>
+        </div>
+
+        <?php if (isset($page['content'])) { print render($page['content']); } ?>
+
+      </div>
+
+      <?php if ( ($page['sidebar_right']) ) : ?>
+      <div class="<?php if ($page['sidebar_left']) { echo "col-md-3";} else { echo "col-md-3"; } ?>">
+        <?php print render($page['sidebar_right']); ?>
+      </div>
+      <?php endif; ?>
+	  </div>
 	  
 	</div>
 
