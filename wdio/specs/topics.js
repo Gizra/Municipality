824cf11a9fe8f5abd260bc79393bf41dbffbc273
nader77 <nader@gizra.com>
2017-06-25 13:33:41 +0300

    Fix all tests.

--- wdio/specs/topics.js
+++ wdio/specs/topics.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 describe('Municipality topics', () => {
   before(() => {
-    browser.url('/municipality-1/taxonomy/term/13?language=he');
+    browser.url('/municipality-1/taxonomy/term/13?user_type=residents&language=he');
   });
 
   it('should show the correct topics', () => {
@@ -26,7 +26,6 @@ describe('Municipality topics', () => {
 
   it('should show all the relevant events for this topic', () => {
     browser.waitForVisible('h4=הצגת ילדים: שבת בבוקר');
-    browser.waitForVisible('h4=סיור קבלנים: אספקה והתקנה של מערכות מיזוג האוויר לבניין העירייה');
   });
 
   it('should show event image if exists', () => {
