24ab2a24dbf5e45eab258969654066fd1fdad9ed
nader77 <nader@gizra.com>
2017-06-27 17:17:08 +0300

    Add editor test.

--- wdio/specs/edit.js
+++ wdio/specs/edit.js
@@ -1,13 +1,18 @@
 const assert = require('assert');
 
 describe('edit page', () => {
-  it('should be able to use ajax on the edit page', () => {
+
+  const loginWithUserNoam = () => {
     browser.url('/user/login');
 
     browser.waitForVisible('#edit-name');
     browser.setValueSafe('#edit-name', 'noam');
     browser.setValueSafe('#edit-pass', 'noam');
     browser.submitForm('#user-login');
+  };
+
+  it('should be able to use ajax on the edit page', () => {
+    loginWithUserNoam();
 
     browser.url('/municipality-1/node/1/edit?language=he');
 
@@ -18,5 +23,25 @@ describe('edit page', () => {
 
     // Make sure the new field has been added.
     browser.waitForVisible('#edit-field-social-links-und-3-title');
+  });
+
+  it('should see the content inside the WYSIWYG editor', () => {
+    browser.waitForVisible('#edit-purl-value');
+
+    // Change HTML format to get the WYSIWYG editor.
+    const htmlFormatSelectBox = $('#edit-field-footer-text-und-0-format--2');
+    htmlFormatSelectBox.selectByValue('filtered_html');
+
+    // Wait for the editor to be visible.
+    browser.waitForVisible('.cke');
+
+    // Check that we have the content inside the editor.
+    browser.element('iframe', (err, el) => {
+      browser.frame(el.value.ELEMENT, () => {
+        browser.getText('.cke_wysiwyg_frame body.cke_editable > p', (err2, text) => {
+          assert.equal('Footer text', text);
+        });
+      });
+    });
   });
 });
