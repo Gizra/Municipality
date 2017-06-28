a23d84aec58c68c529ec03ab77dc6bd161919522
nader77 <nader@gizra.com>
2017-06-28 11:23:27 +0300

    Fix WDIO test

--- wdio/specs/switchers.js
+++ wdio/specs/switchers.js
@@ -1,14 +1,6 @@
-const assert = require('assert');
-
 describe('switchers block', () => {
   it('should see "user types" switcher block on edit pages', () => {
-    browser.url('/user/login');
-
-    browser.waitForVisible('#edit-name');
-    browser.setValueSafe('#edit-name', 'noam');
-    browser.setValueSafe('#edit-pass', 'noam');
-    browser.submitForm('#user-login');
-
+    browser.login('noam');
     browser.url('/municipality-1/node/1/edit?language=he');
 
     browser.waitForVisible('.container .user-types');
