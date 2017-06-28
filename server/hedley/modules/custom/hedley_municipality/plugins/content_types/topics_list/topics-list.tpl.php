163852f145301a3cff2388f3a979f4f90977eb17
nader77 <nader@gizra.com>
2017-06-28 10:12:14 +0300

    Add topics list to homepage.

---
+++ server/hedley/modules/custom/hedley_municipality/plugins/content_types/topics_list/topics-list.tpl.php
@@ -0,0 +1,19 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="divider"></div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-12">
+    <h2 class="center"><?php print $title; ?></h2>
+    <p>
+      <?php foreach ($topics as $topic): ?>
+        <a href="<?php print $topic['link']; ?>" title="<?php print $topic['title']; ?>">
+          <button class="btn btn-borders btn-<?php print $topic['color']; ?> mr-xs mb-sm">
+            <?php print $topic['title']; ?>
+          </button>
+        </a>
+      <?php endforeach; ?>
+    </p>
+  </div>
+</div>
