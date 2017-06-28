0eea06979ba5303857918e1fd6b355086351d9b7
nader77 <nader@gizra.com>
2017-06-28 12:47:28 +0300

    Fix label [skip ci]

--- server/hedley/modules/custom/hedley_municipality/plugins/content_types/topics_list/topics-list.tpl.php
+++ server/hedley/modules/custom/hedley_municipality/plugins/content_types/topics_list/topics-list.tpl.php
@@ -15,9 +15,9 @@
     <h2 class="center"><?php print $title; ?></h2>
     <p>
       <?php foreach ($topics as $topic): ?>
-        <a href="<?php print $topic['link']; ?>" title="<?php print $topic['title']; ?>">
+        <a href="<?php print $topic['link']; ?>" title="<?php print $topic['label']; ?>">
           <button class="btn btn-borders btn-<?php print $topic['color']; ?> mr-xs mb-sm">
-            <?php print $topic['title']; ?>
+            <?php print $topic['label']; ?>
           </button>
         </a>
       <?php endforeach; ?>
