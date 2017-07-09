cd5863c8dcf625afa1aec170fcecec2d94bb925b
nader77 <nader@gizra.com>
2017-07-09 12:04:40 +0300

    Improve promoted content plugin & news template.

--- server/hedley/modules/custom/hedley_municipality/plugins/content_types/promoted_content/promoted-content-news.tpl.php
+++ server/hedley/modules/custom/hedley_municipality/plugins/content_types/promoted_content/promoted-content-news.tpl.php
@@ -8,9 +8,11 @@
 <?php foreach ($items as $item): ?>
   <div class="testimonial testimonial-primary">
     <div class="testimonial-author">
-      <div class="testimonial-author-thumbnail img-thumbnail">
-        <img src="<?php print $item['image_url']; ?>">
-      </div>
+      <?php if ($item['image_url']): ?>
+        <div class="testimonial-author-thumbnail img-thumbnail">
+          <img src="<?php print $item['image_url']; ?>">
+        </div>
+    <?php endif; ?>
       <h3 class="header"><?php print $item['title']; ?></h3>
     </div>
   </div>
