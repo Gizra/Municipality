dcbd63055906fdbd7779dea600375ec42907aa50
nader77 <nader@gizra.com>
2017-08-13 18:41:26 +0300

    Add event page panel. [skip ci]

---
+++ server/hedley/modules/custom/hedley_event/plugins/content_types/event_page/event-page.tpl.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Action content template.
+ */
+?>
+
+<div class="row text-center">
+  <div class="col-xs-12">
+    <h2><?php print t('Events'); ?></h2>
+  </div>
+</div>
+
+<div class="row text-center">
+  <div class="col-xs-12">
+    <h1><?php print $title; ?></h1>
+  </div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+  </div>
+</div>
