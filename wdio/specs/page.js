b022dafee0893387d500ba1941ea22152b4a4919
nedSaf <nader@gizra.com>
2017-10-16 13:08:10 +0300

    Fix WDIO tests

--- wdio/specs/page.js
+++ wdio/specs/page.js
@@ -1,8 +1,6 @@
-const assert = require('assert');
-
 describe('Municipality basic page', () => {
   before(() => {
-    browser.url('/tuba-zangariyye/node/66');
+    browser.url('/tuba-zangariyye/node/67');
   });
 
   it('should show the basic page title and body with the default language: arabic', () => {
@@ -11,19 +9,19 @@ describe('Municipality basic page', () => {
   });
 
   it('should show the base page title and body with the selected language: english', () => {
-    browser.url('/tuba-zangariyye/node/66?language=en');
+    browser.url('/tuba-zangariyye/node/67?language=en');
     browser.waitForVisible('h2=Terms page english');
     browser.waitForVisible('div=Terms page body english');
   });
 
   it('should show the base page title and body with the selected language: arabic', () => {
-    browser.url('/tuba-zangariyye/node/66?language=ar');
+    browser.url('/tuba-zangariyye/node/67?language=ar');
     browser.waitForVisible('h2=Terms page arabic');
     browser.waitForVisible('div=Terms page body arabic');
   });
 
   it('should show the base page title and body with the selected language: hebrew', () => {
-    browser.url('/tuba-zangariyye/node/66?language=he');
+    browser.url('/tuba-zangariyye/node/67?language=he');
     browser.waitForVisible('h2=תנאי שימוש');
     browser.waitForVisible('div=תנאי שימוש עברית');
   });
