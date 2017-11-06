ffb451259cd612a9db7a04576e7137bf8512f7b1
nedSaf <nader@gizra.com>
2017-11-06 10:25:43 +0200

    Add WDIO tests.

--- wdio/specs/action.js
+++ wdio/specs/action.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 
 describe('Municipality action page', () => {
   before(() => {
+    browser.login('noam');
     browser.url('/tuba-zangariyye/node/12?user_type=residents&language=he');
   });
 
@@ -16,5 +17,48 @@ describe('Municipality action page', () => {
     browser.waitForVisible('h2=الحصول على الإقامة');
     browser.waitForVisible('h3=الحصول على تصريح الإقامة');
     browser.waitForVisible('div=ملء النموذج وتقديمه إلى مكاتب المجلس');
+  });
+
+  it('should not be able to add action without a link or a section', () => {
+    // Go to the add new action page.
+    browser.url('/node/add/action?language=en');
+    browser.waitForVisible('label=Title');
+
+    const titleInput = $('#edit-title-field-en-0-value');
+    titleInput.setValue('Test');
+
+    // Click the save button.
+    $('#edit-submit').click();
+
+    // Should see the error message.
+    browser.waitForVisible('.alert-danger');
+  });
+
+  it('should be able to add action with a link', () => {
+    // Go to the add new action page.
+    browser.url('/node/add/action?language=en');
+    browser.waitForVisible('label=Title');
+
+    const titleInput = $('#edit-title-field-en-0-value');
+    titleInput.setValue('Test action');
+
+    const linkInput = $('#edit-field-link-und-0-title');
+    linkInput.setValue('Test link');
+
+    const urlInput = $('#edit-field-link-und-0-url');
+    urlInput.setValue('http://e.com');
+
+    // Click the save button.
+    $('#edit-submit').click();
+
+    // Should not see the error message.
+    assert(!browser.isVisible('.alert-danger'));
+
+    // Should see the node's title.
+    browser.waitForVisible('h2=Test action');
+  });
+
+  after(() => {
+    browser.logout();
   });
 });
