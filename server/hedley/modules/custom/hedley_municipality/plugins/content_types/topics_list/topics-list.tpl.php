f2315603d7ed3e93bccf4ac97f20b52ac2ba2232
nedSaf <nader@gizra.com>
2017-11-06 17:32:48 +0200

    Add topic links [skip ci]

--- server/hedley/modules/custom/hedley_municipality/plugins/content_types/topics_list/topics-list.tpl.php
+++ server/hedley/modules/custom/hedley_municipality/plugins/content_types/topics_list/topics-list.tpl.php
@@ -21,5 +21,12 @@
         </button>
       </a>
     <?php endforeach; ?>
+    <?php if ($add_term_url): ?>
+      <a href="<?php print $add_term_url; ?>" title="<?php print t('Add a new term'); ?>" class="pull-right">
+        <button class="btn btn-primary btn-xs mb-sm">
+          <?php print t('Add a new term'); ?>
+        </button>
+      </a>
+    <?php endif; ?>
   </p>
 </div>
