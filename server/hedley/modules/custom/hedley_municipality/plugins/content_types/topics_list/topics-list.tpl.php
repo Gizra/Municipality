b90ef303533f165156d018ab2166472e419b8a31
nedSaf <nader@gizra.com>
2017-11-08 11:31:54 +0200

    Add visible focus for accessbility.

--- server/hedley/modules/custom/hedley_municipality/plugins/content_types/topics_list/topics-list.tpl.php
+++ server/hedley/modules/custom/hedley_municipality/plugins/content_types/topics_list/topics-list.tpl.php
@@ -15,17 +15,13 @@
   <h2 class="center"><?php print $title; ?></h2>
   <p>
     <?php foreach ($topics as $topic): ?>
-      <a href="<?php print $topic['link']; ?>" title="<?php print $topic['label']; ?>">
-        <button class="btn btn-borders btn-<?php print $topic['color']; ?> mr-xs mb-sm">
-          <?php print $topic['label']; ?>
-        </button>
+      <a href="<?php print $topic['link']; ?>" title="<?php print $topic['label']; ?>" class="btn btn-borders btn-<?php print $topic['color']; ?> mr-xs mb-sm">
+        <?php print $topic['label']; ?>
       </a>
     <?php endforeach; ?>
     <?php if ($add_term_url): ?>
-      <a href="<?php print $add_term_url; ?>" title="<?php print t('Add a new topic'); ?>" class="pull-right">
-        <button class="btn btn-primary btn-xs mb-sm">
-          <?php print t('Add a new topic'); ?>
-        </button>
+      <a href="<?php print $add_term_url; ?>" title="<?php print t('Add a new topic'); ?>" class="pull-right btn btn-primary btn-xs mb-sm"">
+        <?php print t('Add a new topic'); ?>
       </a>
     <?php endif; ?>
   </p>
